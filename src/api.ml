1495ca4fa1e44921130da895cef87b988a98f510
Dominic Price <dominic.price@nottingham.ac.uk>
2014-03-07 15:07:03 +0000

    Updated to match Cohttp API version 0.10.0

--- src/api.ml
+++ src/api.ml
@@ -35,15 +35,13 @@ let error_to_string = function
       
 let get_expect_200 t params path =
   Uri.add_query_params' (Uri.with_path t.uri path) params 
-  |> Client.get >>= function
-    | Some (resp, body) -> 
+  |> Client.get >>= fun (resp, body) -> 
       (match (
         match resp.Response.status with
         | `Code c -> c
         | c -> Code.code_of_status c) with
-      | 200 -> Body.string_of_body body >>= (fun b -> Ok b |> Lwt.return)
+      | 200 -> Body.to_string body >>= (fun b -> Ok b |> Lwt.return)
       | c -> Error (Unexpected_response (200, c, Code.reason_phrase_of_code c)) |> Lwt.return)
-    | None -> Error No_response |> Lwt.return
       
 let get_home_stream ?user_id:(user_id="me") ?since t =
   let params = match since with
